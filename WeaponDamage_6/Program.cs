1547f045f96a909eb4c2507aac0419770f721b00
chajus1 <chajus1@googlemail.com>
2023-01-25 16:43:22 +0000

    Name

--- WeaponDamage_6/Program.cs
+++ WeaponDamage_6/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WeaponDamage_6
+namespace WeaponDamage
 {
     internal class Program
     {
--- WeaponDamage_6/SwordDamage.cs
+++ WeaponDamage_6/SwordDamage.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WeaponDamage_6
+namespace WeaponDamage
 {
     internal class SwordDamage
     {
